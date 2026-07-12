21d638e442558943c3348222fb1554db52b472ad
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "frontdoor_custom_https_configurations_id" {
+  description = "Map of id values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
+  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.id }
+}
 output "frontdoor_custom_https_configurations_custom_https_configuration" {
   description = "Map of custom_https_configuration values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
   value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.custom_https_configuration }
