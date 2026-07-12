e51c0a431bbf981bfffde126bbf269a764b3e2a4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "frontdoor_custom_https_configurations" {
-  description = "All frontdoor_custom_https_configuration resources"
-  value       = azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations
-}
 output "frontdoor_custom_https_configurations_custom_https_configuration" {
-  description = "List of custom_https_configuration values across all frontdoor_custom_https_configurations"
-  value       = [for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : v.custom_https_configuration]
+  description = "Map of custom_https_configuration values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
+  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.custom_https_configuration }
 }
 output "frontdoor_custom_https_configurations_custom_https_provisioning_enabled" {
-  description = "List of custom_https_provisioning_enabled values across all frontdoor_custom_https_configurations"
-  value       = [for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : v.custom_https_provisioning_enabled]
+  description = "Map of custom_https_provisioning_enabled values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
+  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.custom_https_provisioning_enabled }
 }
 output "frontdoor_custom_https_configurations_frontend_endpoint_id" {
-  description = "List of frontend_endpoint_id values across all frontdoor_custom_https_configurations"
-  value       = [for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : v.frontend_endpoint_id]
+  description = "Map of frontend_endpoint_id values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
+  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.frontend_endpoint_id }
 }
 
