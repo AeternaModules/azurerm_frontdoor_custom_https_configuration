a9981f4b34f9cc3ec7bcdbe75c09ac54b5adea8e
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:03:14 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "frontdoor_custom_https_configurations_id" {
 }
 output "frontdoor_custom_https_configurations_custom_https_configuration" {
   description = "Map of custom_https_configuration values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
-  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => v.custom_https_configuration if v.custom_https_configuration != null && length(v.custom_https_configuration) > 0 }
+  value       = { for k, v in azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configurations : k => one(v.custom_https_configuration) if v.custom_https_configuration != null && length(v.custom_https_configuration) > 0 }
 }
 output "frontdoor_custom_https_configurations_custom_https_provisioning_enabled" {
   description = "Map of custom_https_provisioning_enabled values across all frontdoor_custom_https_configurations, keyed the same as var.frontdoor_custom_https_configurations"
